319b01785575dad533fced7e4ce846111fdb177d
MikeSal2 <44678679+MikeSal2@users.noreply.github.com>
2018-11-06 11:50:19 -0500

    No changes

--- Group.js
+++ Group.js
@@ -1,27 +1,11 @@
 class Group {
-  /**
-   * This class will represent a group in a game.
-   * A group can have an arraylist of players and can sort them in ascending
-   * order by their amount of wins
-   */
 
-  constructor() {
-    this.players = [];
-
+  constructor(players) {
+    this.players = players;
     }
 
-    addPlayer(player) {
+    addPlayer() {
       player = new player();
       players.add(player);
-    }
-
-    sortGroup() {
-      players.sort(function(a, b){return a.getWins() - b.getWins()});
-      return players;
-    }
-
-    getWinner() {
-      winner = players.get(0);
-      return winner;
     }
 }
